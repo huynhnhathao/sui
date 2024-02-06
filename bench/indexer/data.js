ed912b8f4265e77add0ad291c0b656ecabd9f27e
github-action-benchmark <github@users.noreply.github.com>
2024-02-06 17:16:31 +0000

    add Benchmark (cargo) benchmark result for 99ccaa83d31afa934b4a36e51f2892ff91ee4975

--- bench/indexer/data.js
+++ bench/indexer/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707181015155,
+  "lastUpdate": 1707239790940,
   "repoUrl": "https://github.com/MystenLabs/sui",
   "entries": {
     "Benchmark": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
             "name": "get_checkpoint",
             "value": 333140,
             "range": "± 23971",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "113186356+Rajwanshi1@users.noreply.github.com",
+            "name": "Rajwanshi1",
+            "username": "Rajwanshi1"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "99ccaa83d31afa934b4a36e51f2892ff91ee4975",
+          "message": "Compute metadata flag to optimize gas.  (#15275)\n\n## Description \r\n\r\nAdded compute_metadata flag to compute metadata only when required by\r\nthe caller. Return val for metadata has been changed to Option<T>.\r\n\r\n## Test Plan \r\n\r\nThis proposed change does'nt affect signature of any public func. Hence,\r\nexisting tests should suffice.\r\n\r\n---\r\nIf your changes are not user-facing and not a breaking change, you can\r\nskip the following section. Otherwise, please indicate what changed, and\r\nthen add to the Release Notes section as highlighted during the release\r\nprocess.\r\n\r\n### Type of Change (Check all that apply)\r\n\r\n- [ ] protocol change\r\n- [ ] user-visible impact\r\n- [ ] breaking change for a client SDKs\r\n- [ ] breaking change for FNs (FN binary must upgrade)\r\n- [ ] breaking change for validators or node operators (must upgrade\r\nbinaries)\r\n- [ ] breaking change for on-chain data layout\r\n- [ ] necessitate either a data wipe or data migration\r\n\r\n### Release notes\r\n\r\n---------\r\n\r\nCo-authored-by: Sarthak Rajwanshi <sarthak.rajwanshi@gmail.com>\r\nCo-authored-by: patrick <patrickkuo@me.com>",
+          "timestamp": "2024-02-06T12:08:54-05:00",
+          "tree_id": "96f29fda158231f0738bd8621b6e80ef41c3dd82",
+          "url": "https://github.com/MystenLabs/sui/commit/99ccaa83d31afa934b4a36e51f2892ff91ee4975"
+        },
+        "date": 1707239786201,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "get_checkpoint",
+            "value": 371959,
+            "range": "± 27281",
             "unit": "ns/iter"
           }
         ]
